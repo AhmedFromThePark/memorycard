9d48ace3722f3ee437d7316faa773f1d214073b9
AhmedFromThePark <73194048+AhmedFromThePark@users.noreply.github.com>
2022-01-22 15:41:45 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const cards = document.querySelectorAll('.grid-item');
+function flipCard() {
+    this.classList.toggle('flip');
+
+}
+cards.forEach(card => card.addEventListener('click', flipCard))
